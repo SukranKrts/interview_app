6cc03dd39473589a523c783cd3c9aa5a67c971b1
SukranKrts <sukran60285@gmail.com>
2023-09-25 12:18:59 +0300

    Grafik düzeltildi.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
 import Appbar from "../components/Appbar";
-import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { DataGrid, GridColDef, GridColumnVisibilityModel, GridFilterModel, GridRowsProp, GridToolbar } from '@mui/x-data-grid';
 import { Avatar, Box, FormControlLabel, Switch } from "@mui/material";
-import { PieChart } from "@mui/x-charts";
+import { BarChart, PieChart, SparkLineChart } from "@mui/x-charts";
 
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 80 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'description', headerName: 'Description', width: 300 },
-    { field: 'series', headerName: 'Series', width: 150 },
-];
-/*const item = [
-    { id: 1, name: 'Agent Mobius', description: '', series: 'Agent Mobius' },
-    { id: 2, name: 'Captain Marvel', description: '', series: 'Captain Marvel' },
-    { id: 3, name: 'Loki', description: '', series: 'Loki' }
-];*/
 
 function HomePage() {
-    const { isAuthenticated, user } = useAuth0();
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 80 },
+        { field: 'name', headerName: 'Name', width: 150 },
+        { field: 'series', headerName: 'Series', width: 150 },
+        { field: 'description', headerName: 'Description', width: 300 },
+    ];
+    const items = [
+        { id: 1, name: 'Agent Mobius', series: 'Agent Mobius', description: '' },
+        { id: 2, name: 'Captain Marvel', series: 'Captain Marvel', description: '' },
+        { id: 3, name: 'Loki', series: 'Loki', description: '' },
+        { id: 4, name: 'Monica Rambeau', series: 'Monica Rambeau', description: '' }
+    ];
+    const rows: GridRowsProp = items;
 
-    const [items, setİtems] = useState([]);
+    //const [items, setİtems] = useState([]);
 
-    React.useEffect(() => {
+    /*React.useEffect(() => {
         const response = axios('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=113898c5419a6aee5b354a4939a21f39&hash=54ca4cbf481ade7f7f6fff99d7cf0100')
             .then(response => setİtems(response.data.data.results))
 
-    });
-    const rows: GridRowsProp = items;
+    });*/
+
     const [filterModel, setFilterModel] = React.useState<GridFilterModel>({
         items: [],
         quickFilterExcludeHiddenColumns: true,
@@ -69,18 +69,14 @@ function HomePage() {
                     </Box>
                 </Box>
             </div>
-            <div style={{ position: "absolute", left: '60%', top: '60%' }}>
-                <PieChart
-                    series={[
-                        {
-                            data: [
-                                { id: 0, value: 10, label: 'series A' },
-                            ]
-                        },
-                    ]}
-                    width={400}
-                    height={200}
-                /></div>
+            <div style={{ position: "absolute", left: '60%', top: '15%' }}>
+                <BarChart
+                    xAxis={[{ scaleType: 'band', data: items.map(item => (item.name)) }]}
+                    series={[{ data: items.map(item => (item.name.length)) }]}
+                    width={500}
+                    height={300}
+                />
+            </div>
         </div>
 
     );
